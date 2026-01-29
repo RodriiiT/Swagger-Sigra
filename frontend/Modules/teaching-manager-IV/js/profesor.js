767ac1d0530f650bb8378c399623317e95612aee
Rodrigo <rodrileogabriel@gmail.com>
2026-01-29 16:57:41 -0400

    Fix anchor tag syntax for resource link

--- frontend/Modules/teaching-manager-IV/js/profesor.js
+++ frontend/Modules/teaching-manager-IV/js/profesor.js
@@ -79,7 +79,7 @@ async function cargarMaterialApoyo() {
                     <h4 style="margin:10px 0; font-size:1rem;">${r.title}</h4>
                     <p style="font-size:0.75em; color:#666; margin-bottom:10px;">Tipo: ${r.resource_type}</p>
                     
-                    <a href="https://sigra.irissoftware.lat/uploads/resources/${r.file_path_or_url.split('\\').pop()" target="_blank" 
+                    <a href="https://sigra.irissoftware.lat/uploads/resources/${r.file_path_or_url.split('\\').pop()}" target="_blank" 
                        class="btn-primario" style="text-decoration:none; display:inline-block; font-size:0.85rem; padding:5px 10px; background:#123E6A; color:white; border-radius:5px;">
                         <i class="fas fa-external-link-alt"></i> Ver Recurso
                     </a>
